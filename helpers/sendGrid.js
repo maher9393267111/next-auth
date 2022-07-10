cdb2d6cf9a034138c91b1a0cfacd5d4188903203
maher <gomemahero.com>
2022-07-10 04:46:34 +0300

    send grid start wooork

--- helpers/sendGrid.js
+++ helpers/sendGrid.js
@@ -1,7 +1,7 @@
 // use sendGrid to send email
 import sgmail from "@sendgrid/mail"
 
-sgmail.setApiKey(process.env.SENDGRID_API_KEY)
+sgmail.setApiKey('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')
 
  export const userWelcomemail=(email,name,message)=>{
 
@@ -10,6 +10,7 @@ sgmail.setApiKey(process.env.SENDGRID_API_KEY)
 
         sgmail.send({
             to:email,
+         //   to:'maher9911133@gmail.com',
             from:'gomemahero@gmail.com',  // this is sendgrid account gmail  ---> send t onother email account
             subject:'reset password from here', 
             text:message

54a02a4227c0c83c7c9f1509c315e04d7b024e08
maher <gomemahero.com>
2022-07-10 04:56:02 +0300

    fix bogs try

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react'
-import { Head } from 'next/document';
+//import { Head } from 'next/document';
 import { Provider } from 'next-auth/react'
 import Layout from "../components/Layout"
 import { ToastContainer } from "react-toastify"
